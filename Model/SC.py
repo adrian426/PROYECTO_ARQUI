eb1036c0cc49b6e411d1899f83c37f9debcbd6a7
Ariel Stiven Arias Rojas <b60685@ecci.ucr.ac.cr>
2019-06-24 11:19:06 -0600

    Se resuelve el fallo de cache en el sc

--- Model/SC.py
+++ Model/SC.py
@@ -1,4 +1,5 @@
 from StatesEnum import StatesEnum
+from DataMemory.DataBlock import DataBlock
 
 
 DATA_BUS_OPERATION_CLOCK_CYCLES = 32
@@ -60,11 +61,16 @@ class SC:
                     return LOCK_ERROR
             # Check if the block is invalid or not in self cache, or if the RL isnt the needed value
             if (mem_address_on_cache and mem_address_state_on_cache == StatesEnum.INVALID) or \
-                    (not mem_address_on_cache) or (self.__core_instance.get_self_rl != mem_add_to_store):
+                    (not mem_address_on_cache):
+
+                # or (self.__core_instance.get_self_rl != mem_add_to_store):
                 # Not on self cache or invalid on self cache, or the RL isnt the needed value
-                # Set x2 to 0
-                self.__core_instance.set_register(source_registry, 0)
-                return total_execution_clock_cycles
+
+                total_execution_clock_cycles += self.solve_cache_miss(mem_add_to_store)
+                if self.__core_instance.get_self_rl != mem_add_to_store:
+                    # Set x2 to 0
+                    self.__core_instance.set_register(source_registry, 0)
+                    return total_execution_clock_cycles
         else:
             # Can't get self cache
             return LOCK_ERROR
@@ -75,5 +81,41 @@ class SC:
         self.__core_instance.change_word_value_data_cache(mem_add_to_store, value_to_store)
         # Returns the execution time
         return total_execution_clock_cycles
+
+    # Method to solve cache miss, assumes that only self cache is locked
+    def solve_cache_miss(self, m_address):
+        clock_cycles = 0
+        # LOCK OTHER CORE CACHE AND DATA BUS
+        if self.__core_instance.acquire_other_and_data_bus_locks():
+            # LOCKS ACQUIRED + 2 Other cache +32 Data bus
+            clock_cycles += CONSULT_OTHER_CACHE_CLOCK_CYCLES + DATA_BUS_OPERATION_CLOCK_CYCLES
+            if self.__core_instance.get_if_memory_address_on_other_cache(m_address) and \
+                    self.__core_instance.get_memory_address_state_on_other_cache(m_address) == StatesEnum.MODIFIED:
+                # Memory address is on the other core cache and other core cache block is modified
+                # Store the other core block and get the new block
+                data_block_to_insert = DataBlock(0)
+                data_block_to_insert.copy_data_block(
+                    self.__core_instance.store_other_core_data_cache_block_on_main_memory(
+                        m_address, StatesEnum.INVALID))
+
+                # Invalidate the RL on the other core, in the case that it was using a lr
+                self.__core_instance.invalidate_other_core_rl(m_address)
+
+                clock_cycles += self.__core_instance.store_block_on_self_cache(
+                    StatesEnum.SHARED, m_address, data_block_to_insert)
+                return clock_cycles
+            # Memory address isn't in the other core or isn't modified
+            # LOCK RELEASED OTHER CORE CACHE
+            self.__core_instance.release_other_core_cache()
+            # Only need to load to self cache + 32
+            main_memory_data_block = self.__core_instance.get_data_block(m_address)
+            data_to_insert = DataBlock(0)
+            data_to_insert.copy_data_block(main_memory_data_block)
+            clock_cycles += self.__core_instance.store_block_on_self_cache(StatesEnum.SHARED, m_address,
+                                                                           data_to_insert)
+            return clock_cycles
+        else:
+            # Cant get the other core cache or data bus
+            return LOCK_ERROR
 
 
