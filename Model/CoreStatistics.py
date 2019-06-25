e3a50246edd1e7a07ea03e630c6a8329893af841
Bailin Huang <bailinhuangq@hotmail.com>
2019-06-24 20:48:12 -0600

    core and hilillo statistics

---
+++ Model/CoreStatistics.py
@@ -0,0 +1,4 @@
+class CoreStatistics:
+
+    def __init__(self, id):
+        self.id = id
---
+++ Model/HililloStatistics.py
@@ -0,0 +1,11 @@
+class HililloStatistics:
+
+    def __init__(self, id, core_id, registers, cycles):
+        self.id = id
+        self.core_id = core_id
+        self.registers = registers
+        self.cycles = cycles
+
+
+
+
